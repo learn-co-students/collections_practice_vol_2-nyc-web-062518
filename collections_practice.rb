e8a00b2d1016c78f2f90482668acbe5874ca8751
Daniel Ran <danielr1735@gmail.com>
2018-06-23 06:43:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
-# your code goes here
+def first_wa(array)
+  array.find { |element| element[0..1] == "wa" }
+end
+
+def count_elements(array)
+  counts = []
+  until array == []
+    temp = array.select {|element| element != array[0]}
+    count = array.length - temp.length
+    array[0][:count] = count
+    counts.push(array[0])
+    array = temp
+  end
+  counts
+end
+
+
+def begins_with_r(array)
+  !array.any? do |element|
+    element[0] != 'r'
+  end
+end
+
+def contain_a(array)
+  array.select do |element|
+    element.include?('a')
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |element|
+    !element.is_a?(String)
+  end
+end
